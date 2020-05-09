38d07fa7716b96094458146ff83263fc49c00e59
Gera <grsb99@gmail.com>
2020-05-09 10:04:50 +0200

    frontend: login page

--- api/src/app/login/login.component.ts
+++ api/src/app/login/login.component.ts
@@ -5,8 +5,27 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   template: `
-    <div>
-      <button (click)="goLogin()">Login</button>
+    <div class="loginComponent">
+      <div class="headerHolder">
+        <div class="logoHolder">
+            <img class="logo" src="/assets/login/undraw_bitcoin2_ave7.png" alt="">
+          <div class="createdBy">
+            <img class="avatar" src="/assets/login/avatar.png" alt="">
+            <div class="createdByText"><p>created by Geresdi Bence</p></div>
+          </div>
+        </div>
+      </div>
+      <div class="mainContent">
+        <div class="mainImgHolder">
+          <img src="/assets/login/undraw_btc_p2p_lth5.png" alt="">
+        </div>
+        <div class="mainTextHolder">
+          <h1>Crypto Trader website</h1>
+          <h2>Login to Your Account!</h2>
+          <button (click)="goLogin()">Login</button>
+
+        </div>
+      </div>
     </div>
   `,
   styleUrls: ['./login.component.css']
