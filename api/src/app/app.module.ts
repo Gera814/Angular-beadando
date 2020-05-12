a774a8eeb03881d92af122ba7f9318c5fa00d11f
Gera <grsb99@gmail.com>
2020-05-13 00:20:27 +0200

    fix

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TetsComponent } from './tets/tets.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
-import {EmployeeService} from './employee.service';
 import {HttpClientModule} from '@angular/common/http';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
 import { BuyComponent } from './buy/buy.component';
@@ -17,9 +13,6 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 @NgModule({
   declarations: [
     AppComponent,
-    TetsComponent,
-    EmployeeListComponent,
-    EmployeeDetailsComponent,
     CryptoListComponent,
     routingComponents,
     BuyComponent,
@@ -32,7 +25,6 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
         FormsModule,
         FontAwesomeModule
     ],
-  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- api/src/app/services/data.service.ts
+++ api/src/app/services/data.service.ts
@@ -38,52 +38,8 @@ export class DataService {
     localStorage.setItem('buyed', JSON.stringify(this.buyedCryptos));
 
   }
-
-
-
   errorHandler(error: HttpErrorResponse){
     return throwError(error.message);
   }
-  /*
-  constructor() {
-    localStorage.removeItem('data');
-    const users = [];
-    for (let i = 0; i < 10; i++) {
-      const user = new User();
-      user.id = 'id-id-id-' + i;
-      user.name = 'Name ' + i;
-      user.birthDate = new Date();
-      user.city = 'City ' + i;
-      users.push(user);
-    }
-    localStorage.setItem('data', JSON.stringify(users));
-  }
-
-  public list(): User[] {
-    return JSON.parse(localStorage.getItem('data')) as User[];
-  }
-
-  public add(user: User) {
-    const data = this.list();
-    data.push(user);
-    localStorage.setItem('data', JSON.stringify(data));
-  }
-
-  public save(user: User) {
-    let data = this.list();
-    data = data.filter(value => value.id !== user.id);
-    data.push(user);
-    localStorage.setItem('data', JSON.stringify(data));
-  }
-
-  public findById(id: string) {
-    const data = this.list();
-    for (const d of data) {
-      if (d.id === id) {
-        return d;
-      }
-    }
-    return undefined;
-  }*/
 
 }
